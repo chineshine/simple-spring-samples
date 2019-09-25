6ac39c5ce1ca1487404f172765a633f4cca36e7c
chineshine <chineshine@outlook.com>
2019-09-25 09:37:50 +0800

    init

---
+++ src/main/java/c/s/sample/simplespringsample/SimpleSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package c.s.sample.simplespringsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringSampleApplication.class, args);
+	}
+
+}
