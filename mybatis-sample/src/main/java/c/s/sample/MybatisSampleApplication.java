cbcf10b78a920666cc4b16a6d46ecded69478c30
chineshine <chineshine@outlook.com>
2019-11-26 11:29:27 +0800

    add mybatis sample

---
+++ mybatis-sample/src/main/java/c/s/sample/MybatisSampleApplication.java
@@ -0,0 +1,13 @@
+package c.s.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSampleApplication.class, args);
+	}
+
+}
