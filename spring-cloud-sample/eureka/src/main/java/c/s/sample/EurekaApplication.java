7831087bcd024fa80544738d5a941b61f1fec2c5
chineshine <chineshine@outlook.com>
2019-12-31 16:45:55 +0800

    cloud sample add eureka

---
+++ spring-cloud-sample/eureka/src/main/java/c/s/sample/EurekaApplication.java
@@ -0,0 +1,19 @@
+package c.s.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chineshine
+ * @date 2019年12月31日
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
