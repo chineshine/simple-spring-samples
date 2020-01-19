7b0ac130053c53f2f47379f3eb3ca66f8f16b644
chineshine <chineshine@outlook.com>
2020-01-19 09:52:16 +0800

    cpmplete resource  server

--- spring-oauth2-sample/resource-server/src/main/java/c/s/sample/config/ResourceServerConfig.java
+++ spring-oauth2-sample/resource-server/src/main/java/c/s/sample/config/ResourceServerConfig.java
@@ -1,18 +1,9 @@
 package c.s.sample.config;
 
-import java.net.URL;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author chineshine
@@ -20,44 +11,18 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
  *
  */
 @Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter  {
 
-	@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-	String jwkSetUri;
-
-	private String checkTokenEndPoint = "http://localhost:8080/oauth/token_key";
-	
-	private String remoteTokenKeyEndPoint = "";
-	
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//		resources.
-	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 //		http.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer(oauth2ResourceServer-> oauth2ResourceServer.jwt(jwt->jwt.jwkSetUri(jwkSetUri)));
-		http.authorizeRequests(authRequest -> authRequest.anyRequest().authenticated()).oauth2ResourceServer(
-				oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> jwt.decoder(jwtDecoder())));
-	}
-
-	@Bean
-	JwtDecoder jwtDecoder() {
-		return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-	}
-	
-	@Bean
-	RemoteTokenServices remoteTokenServices() {
-		RemoteTokenServices tokenServices = new RemoteTokenServices();
-		tokenServices.setCheckTokenEndpointUrl(checkTokenEndPoint);
-		return tokenServices;
-	}
-	
-	JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-		accessTokenConverter.setVerifierKey("");
-		return accessTokenConverter;
+//		http.authorizeRequests(authRequest -> authRequest.anyRequest().authenticated()).oauth2ResourceServer(
+//				oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> jwt.decoder(jwtDecoder())));
+		http.authorizeRequests().anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer().jwt();
 	}
 
 	
