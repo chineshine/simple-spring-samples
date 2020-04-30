91aae6102f730e15694cc0016d3c46c654485809
chineshine <chineshine@outlook.com>
2020-04-30 17:19:11 +0800

    --

---
+++ spring-jpa-sample/src/main/java/c/s/sample/applying/entity/ApplyingForm.java
@@ -0,0 +1,14 @@
+package c.s.sample.applying.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class ApplyingForm {
+	
+	private ApplyingType applyingType;
+
+	private String ApplyingName;
+}
---
+++ spring-jpa-sample/src/main/java/c/s/sample/applying/entity/ApplyingType.java
@@ -0,0 +1,17 @@
+package c.s.sample.applying.entity;
+
+/**
+ * @author chineshine
+ * @since  2020年4月30日
+ */
+public enum ApplyingType {
+	
+	// 休假申请
+	VACATION,
+	
+	// 报销
+	REFUND
+	
+	;
+
+}
